782143a46b76181c2ab156209462bb3e4bbbb17b
Jhonny Lima <jlima@Jhonnys-MacBook-Pro.local>
2021-06-07 20:12:52 -0300

    + Add: Pages and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Home | ig.news</title>
+        <title>Home | ig.news</title>˘
       </Head>
-      <h1>
-        Hello, Next!
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+        
+      </main>
     </>
   )
 }
